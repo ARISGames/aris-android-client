a6d28ee2a98da5aa37753950275f4d6a6ea6de09
Scott Morison <smorison@uoregon.edu>
2015-07-30 11:22:45 -0700

    Trying to push to git

--- Aris_Android/app/src/main/java/edu/uoregon/casls/aris_android/AppUtils.java
+++ Aris_Android/app/src/main/java/edu/uoregon/casls/aris_android/AppUtils.java
@@ -10,11 +10,12 @@ import android.net.NetworkInfo;
 public class AppUtils {
 	// scott was here and here
 	public static final String SERVER_URL_BASE = "http://10.223.178.105"; //localhost
-	public static final String SERVER_URL_MOBILE = SERVER_URL_BASE + "/server/json.php";
+	public static final String SERVER_URL_MOBILE = SERVER_URL_BASE + "/server/json.php/";
 	public static final String LOGTAG = "ARIS_ANDROID";
 
 	public final static Boolean DEBUG_ON = true; //todo: Make sure to turn this off for release version
-	public final static Boolean FAKE_GOOD_LOGIN = true; //todo: Make sure to turn this off for release version
+	public final static Boolean FAKE_GOOD_LOGIN = false; //todo: Make sure to turn this off for release version
+	public static final String TAG_SERVER_SUCCESS = "success";
 
 	public static boolean isNetworkAvailable(Context context) {
 		boolean haveConnectedWifi = false;
