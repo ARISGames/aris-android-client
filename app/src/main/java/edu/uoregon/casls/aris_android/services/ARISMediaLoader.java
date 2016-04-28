099cd13d564daad5ba06be3d17296c981c2fbf68
Scott Morison <smorison@uoregon.edu>
2016-04-28 08:47:53 -0700

    MediaLoader needed a lot of TLC. It had several missing and unfinished methods.

--- app/src/main/java/edu/uoregon/casls/aris_android/services/ARISMediaLoader.java
+++ app/src/main/java/edu/uoregon/casls/aris_android/services/ARISMediaLoader.java
@@ -8,10 +8,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
-import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.webkit.URLUtil;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.BinaryHttpResponseHandler;
@@ -20,7 +18,9 @@ import com.loopj.android.http.RequestParams;
 import org.apache.http.Header;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -70,96 +70,85 @@ public class ARISMediaLoader {
 		if (mr.media.thumb != null) { this.mediaLoadedForMR(mr); }
 		else if (mr.media.data != null) { this.deriveThumbForMR(mr); }
 		else if (mr.media.localURL() != null) { // get from the file if it already has been loaded
-//			mr.media.data = dataWithContentsOfURL:mr.media.localURL;
 			mr.media.data = BitmapFactory.decodeFile(mr.media.localURL().getPath());//.decodeStream(mr.media.localURL.openConnection().getInputStream());
 		}
 		else if (mr.media.remoteURL() != null) { // todo: call a pollServer type method to get media data, but one that can handle load failure and schedule to reload.
-			// set upa a server request for the call to get Media data
-//			NSURLRequest request = NSURLRequest requestWithURL:mr.media.remoteURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0;
-//			if (mr.connection) mr.connection cancel; // if it's already defined, bail.
-//			mr.data = NSMutableData alloc initWithCapacity:2048; // allocate defined size for the data property Android necessary?
-//			mr.connection = NSURLConnection alloc initWithRequest:request delegate:self; // fill in the connection request params with a delegate
-			// the key for this map is the media "url" itself, eg:"http://arisgames.org/server/gamedatav2/64/aris8e4de40283cdc5d1aefbbf93d7df82f3.jpg"
-//			dataConnections setObject:mr forKey:mr.connection.description; // add this connection to the array (of currently active connections)
-			// todo: may need to make this an async req. otherwise it might bog down the main thread -sem
-				// fixme: NPE here when loading ECOPod.
-//			URL remoteURL = mr.media.remoteURL();
-			pollServerForMediaWithRemoteURL(mr.media);
-//				URLConnection connection = remoteURL.openConnection();
-//				connection.setConnectTimeout(6000);
-//				InputStream inputStream = new BufferedInputStream(connection.getInputStream()); // fixme NPE
-//				mr.media.data = BitmapFactory.decodeStream(inputStream); // for server retrieval of local file
-
+			// set up an async server request to get Media data
+			pollServerForMediaWithRemoteURL(mr);
 		}
 		else if (mr.media.remoteURL() == null) { this.loadMetaDataForMR(mr); }
 	}
 
-	private void pollServerForMediaWithRemoteURL(Media media) {
-		RequestParams rqParams = new RequestParams();
+	private void pollServerForMediaWithRemoteURL(final MediaResult mediaResult) {
 		final Context context = mGamePlayAct;
-		final String remoteURL = media.remoteURL().toString();
+		final String remoteURL = mediaResult.media.remoteURL().toString();
 		String[] allowedContentTypes = new String[]{"image/png", "image/jpeg", "image/gif",
 				"audio/mp4", "audio/mpeg", "video/mov", "video/mpeg", "video/mp4", "video/JPEG"};
 		if (AppUtils.isNetworkAvailable(mGamePlayAct.getApplicationContext())) {
 			// todo: loading status bar here.
-			AsyncHttpClient client = new AsyncHttpClient();
-//			static String reqCall
-			Log.d(AppConfig.LOGTAG, getClass().getSimpleName() + "pollServerForMediaWithRemoteURL AsyncHttpClient Sending Req for Media Data: " + media.remoteURL());
-			client.get(context, remoteURL, new BinaryHttpResponseHandler(allowedContentTypes /*, looper here? */) { // the looper might be able to handle failed attempts?
+			// showProgress(true);
+
+			if (mediaResult.connection != null) mediaResult.connection.cancelRequests(context, true);
+			mediaResult.connection = new AsyncHttpClient(); // storing client ref in the MR so it can be tracked and cancelled if necessary.
+			mediaResult.connection.setTimeout(6000); //set timeout for 60 sec. (6000ms)
+			Log.d(AppConfig.LOGTAG, getClass().getSimpleName() + "pollServerForMediaWithRemoteURL AsyncHttpClient Sending Req for Media Data: " + mediaResult.media.remoteURL());
+			mediaResult.connection.get(context, remoteURL, new BinaryHttpResponseHandler(allowedContentTypes /*, looper here? */) { // the looper might be able to handle failed attempts?
 				@Override
 				public void onSuccess(int statusCode, Header[] headers, byte[] mediaBytes) {
-					processLoadedBitmapForMedia(mediaBytes, media);
+					processLoadedBitmapForMR(mediaBytes, mediaResult);
 				}
-
 				@Override
 				public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
-					processFailedMediaLoad(media);
+					processFailedMRLoad(mediaResult);
 				}
+//				@Override
+//				public void onProgress(int remaining, int total) {
+////					Log.d(AppConfig.LOGTAG, getClass().getSimpleName() + "AsyncHttpClient Progress for Req: " + requestApi + ". Progress: " + remaining + "/" + total);
+//					// todo: set up progress bars of some sort for each request.
+//				}
 			});
 		}
 		else {
-			// booboo.
 			// todo: handle network unavailable.
 		}
 	}
 
-	private String processLoadedBitmapForMedia(byte[] mediaBytes, Media mediaToLoad) {
-		// save binary to a local file.
-		// enter this media data into database and a local URI to the file (Should there
-		// already be a row for this from prior entry or upstream empty placeholder stub?)
+	private void processLoadedBitmapForMR(byte[] mediaBytes, MediaResult mr) {
+		// save the returned data directly into the media.data field.
+		mr.data = BitmapFactory.decodeByteArray(mediaBytes, 0, mediaBytes.length);
+		mr.media.data = mr.data; // saving the data in two places or are these sharing a reference? Hopefully the latter.
+		// save to local file
 		ContextWrapper cw = new ContextWrapper(mGamePlayAct);
 		// path to /data/data/appName/app_data/gameMedia_(game_id)
 		File directory = cw.getDir("gameMedia_" + mGamePlayAct.mGame.game_id, Context.MODE_PRIVATE);
 		// Create directory with file.
-		File mypath = new File(directory, mediaToLoad.media_id + "." + mediaToLoad.fileExtension());
+		File localMediaPath = new File(directory, mr.media.media_id + "." + mr.media.fileExtension());
 
 		FileOutputStream fos = null;
-
-		// TODO: We want to save the returned data directly into the media.data field.
 		try {
-			fos = new FileOutputStream(mypath);
+			fos = new FileOutputStream(localMediaPath);
 			fos.write(mediaBytes);
 			fos.close();
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		mr.media.setPartialLocalURL(localMediaPath.getAbsolutePath());
+		mGamePlayAct.mMediaModel.addOrUpdateMediaCD(mr.media.mediaCD); // todo: not sure if this is required or desired here. Remove if it's wrong.
+
 		try {
-			mediaToLoad.localURL = new URL(mypath.getAbsolutePath());
+			mr.media.localURL = new URL(localMediaPath.getAbsolutePath());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-
-
-
-		// update DB to reflect new local URL
-//		mGamePlayAct.mMediaModel.addOrUpdateMediaCD(mediaCDToLoad);
-
-		return mypath.getAbsolutePath();
 	}
 
-	private void processFailedMediaLoad(Media mediaToLoad) {
-		// todo: finish me
+	private void processFailedMRLoad(MediaResult mediaToLoad) {
+		// todo: handle download fail.
 	}
 
 	public void loadMetaDataForMR(MediaResult mr) {
@@ -169,25 +158,7 @@ public class ARISMediaLoader {
 			if (existingMR.media.media_id() == mr.media.media_id()) { // this makes the bold assumption that mediaid is not 0.
 				// If mediaresult already exists, merge delegates to notify rather than 1.Throwing new request out (need to keep delegate) or 2.Redundantly requesting
 //				existingMR.delegateHandles = existingMR.delegateHandles arrayByAddingObjectsFromArray:mr.delegateHandles;
-//				vvvvv fixme vvvvv;
-				existingMR.delegateHandles.addAll(mr.delegateHandles); //FIXME: make this work w/o delegates please. NPE here.
-				/* 03-03 14:13:30.424 edu.uoregon.casls.aris_android E/AndroidRuntime: FATAL EXCEPTION: main
-                                                                    Process: edu.uoregon.casls.aris_android, PID: 11838
-                                                                    java.lang.NullPointerException
-                                                                        at edu.uoregon.casls.aris_android.services.ARISMediaLoader.loadMetaDataForMR(ARISMediaLoader.java:108)
-                                                                        at edu.uoregon.casls.aris_android.services.ARISMediaLoader.loadMediaFromMR(ARISMediaLoader.java:97)
-                                                                        at edu.uoregon.casls.aris_android.services.ARISMediaLoader.loadMedia(ARISMediaLoader.java:71)
-                                                                        at edu.uoregon.casls.aris_android.models.MediaModel.deferedLoadMedia(MediaModel.java:245)
-                                                                        at edu.uoregon.casls.aris_android.models.MediaModel$1.run(MediaModel.java:236)
-                                                                        at android.os.Handler.handleCallback(Handler.java:733)
-                                                                        at android.os.Handler.dispatchMessage(Handler.java:95)
-                                                                        at android.os.Looper.loop(Looper.java:146)
-                                                                        at android.app.ActivityThread.main(ActivityThread.java:5694)
-                                                                        at java.lang.reflect.Method.invokeNative(Native Method)
-                                                                        at java.lang.reflect.Method.invoke(Method.java:515)
-                                                                        at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:1291)
-                                                                        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1107)
-                                                                        at dalvik.system.NativeStart.main(Native Method)*/
+				existingMR.delegateHandles.addAll(mr.delegateHandles);
 				return;
 			}
 		}
@@ -207,8 +178,6 @@ public class ARISMediaLoader {
 	//  .........................GamePlayActivity.requestGameData()
 	//  .........................GamePlayActivity.onStart() <-- loading sequence at start of game (or continue game)
 	public void retryLoadingAllMedia() {
-		// this is where we need to walk through the list of media that need to be loaded from server
-
 		// walk through list of all media meta data MediaModel.mediaIdsToLoad array (ids of urls that need to gat their (binary) data from server)
 		for (int mediaIdToLoad : mGamePlayAct.mMediaModel.mediaIDsToLoad) {
 			// dispatch an async service to try and load this data into a MediaResult obj
@@ -219,50 +188,6 @@ public class ARISMediaLoader {
 		}
 	}
 
-	//		//do the ol' switcheroo so we wont get into an infinite loop of adding, removing, readding, etc...
-//		NSMutableArray oldMetaConnections = metaConnections;
-//		metaConnections = NSMutableArray alloc initWithCapacity:10;
-//
-//		MediaResult mr;
-//		while(oldMetaConnections.count > 0)
-//		{
-//			mr = oldMetaConnections objectAtIndex:0;
-//			mr.media = mGamePlayAct.mMediaModel.mediaForId(mr.media.media_id); //_MODEL_MEDIA_ mediaForId:mr.media.media_id;
-//			oldMetaConnections removeObjectAtIndex:0;
-//			this.loadMediaFromMR(mr);
-//		}
-//	}
-//
-//	public void connection(URLConnection c, Bitmap d)
-//	{
-//		MediaResult mr = ;
-//		if (!(mr = dataConnections objectForKey:c.description)) return;
-//		mr.data = d;// appendData:d;
-//	}
-//
-//	public void connectionDidFinishLoading(URLConnection c)
-//	{
-//		MediaResult mr; if(!(mr = dataConnections objectForKey:c.description)) return;
-//		dataConnections removeObjectForKey:c.description;
-//		mr.media.data = mr.data;
-//		mr cancelConnection;//MUST do this only AFTER data has already been transferred to media
-//
-//		//short names to cope with obj-c verbosity
-//		NSString g = NSString stringWithFormat("%ld",mr.media.game_id; //game_id as string
-//		NSString f = mr.media.remoteURL absoluteString componentsSeparatedByString("/" lastObject stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding; //filename
-//
-//		NSString newFolder = _ARIS_LOCAL_URL_FROM_PARTIAL_PATH_(g);
-//		if(!NSFileManager defaultManager fileExistsAtPath:newFolder isDirectory:nil)
-//		NSFileManager defaultManager createDirectoryAtPath:newFolder withIntermediateDirectories:YES attributes:nil error:nil;
-//		mr.media setPartialLocalURL:NSString stringWithFormat("%@/%@",g,f;
-//		mr.media.data writeToURL:mr.media.localURL options:nil error:nil;
-//		mr.media.localURL setResourceValue:NSNumber numberWithBool:YES forKey:NSURLIsExcludedFromBackupKey error:nil;
-//
-//		_MODEL_MEDIA_ saveAlteredMedia:mr.media;//not as elegant as I'd like...
-//		_ARIS_LOG_(@"Media loader  : Media id:%ld loaded:%@",mr.media.media_id,mr.media.remoteURL);
-//		this.loadMediaFromMR:mr;
-//	}
-//
 	public void mediaLoadedForMR(MediaResult mr) {
 		//This is so ugly. See comments in ARISDelegateHandle.h for reasoning
 		for (int i = 0; i < mr.delegateHandles.size(); i++) {
@@ -346,42 +271,19 @@ public class ARISMediaLoader {
 			client.get(context, requestURL, new BinaryHttpResponseHandler(allowedContentTypes /*, looper here? */) { // the looper might be able to handle failed attempts?
 				@Override
 				public void onSuccess(int statusCode, Header[] headers, byte[] mediaBytes) {
-					processLoadedMediaForCD(mediaBytes, mediaCDToLoad);
+					processLoadedMediaForCDToLocalFile(mediaBytes, mediaCDToLoad);
 				}
 
 				@Override
 				public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
 					processFailedMediaForCDLoad(mediaCDToLoad);
 				}
-			});
-
-//			client.post(context, requestURL, entity, "application/json", new JsonHttpResponseHandler() {
-//				@Override
-//				public void onSuccess(int statusCode, Header[] headers, JSONObject jsonReturn) {
-////					showProgress(false);
-//					try {
-//						// todo: custom response handler for media data
-//						mGamePlayAct.mResposeHandler.processJsonHttpResponse(requestURL, AppConfig.TAG_SERVER_SUCCESS, jsonReturn);
-//					} catch (JSONException e) {
-//						e.printStackTrace();
-//					}
-//				}
-//				@Override
-//				public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-//					Log.w(AppConfig.LOGTAG, getClass().getSimpleName() + "AsyncHttpClient failed server call. ", throwable);
-////					showProgress(false);
-//					Toast t = Toast.makeText(mGamePlayAct.getApplicationContext(), "There was a problem receiving data from the server. Please try again, later.",
-//							Toast.LENGTH_SHORT);
-//					t.setGravity(Gravity.CENTER, 0, 0);
-//					t.show();
-//					super.onFailure(statusCode, headers, responseString, throwable);
-//				}
 //				@Override
 //				public void onProgress(int remaining, int total) {
 ////					Log.d(AppConfig.LOGTAG, getClass().getSimpleName() + "AsyncHttpClient Progress for Req: " + requestApi + ". Progress: " + remaining + "/" + total);
 //					// todo: set up progress bars of some sort for each request.
 //				}
-//			});
+			});
 		}
 		else {
 			// todo: what to do when internet is unavailable. A toast is not what we want here. May even want to test connectivity in calling method.
@@ -397,7 +299,7 @@ public class ARISMediaLoader {
 	     Android replacement of connectionDidFinishLoading();
 	     Save data to local file, and populate the localURL with file location
     */
-	private String processLoadedMediaForCD(byte[] mediaBytes, MediaCD mediaCDToLoad) {
+	private String processLoadedMediaForCDToLocalFile(byte[] mediaBytes, MediaCD mediaCDToLoad) {
 		// save binary to a local file.
 		// enter this media data into database and a local URI to the file (Should there
 		// already be a row for this from prior entry or upstream empty placeholder stub?)
--- app/src/main/java/edu/uoregon/casls/aris_android/services/MediaResult.java
+++ app/src/main/java/edu/uoregon/casls/aris_android/services/MediaResult.java
@@ -2,6 +2,8 @@ package edu.uoregon.casls.aris_android.services;
 
 import android.graphics.Bitmap;
 
+import com.loopj.android.http.AsyncHttpClient;
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Date;
@@ -14,12 +16,11 @@ import edu.uoregon.casls.aris_android.media.ARISDelegateHandle;
  * Created by smorison on 11/18/15.
  */
 public class MediaResult {
-	public Media media;
-	public Bitmap data;  // todo Bitmap or ByteBuffer?
-	public URL url;
-	// todo convert the NSURLConnection below to something digestible by java.
-	// todo   heres a link that might help: https://books.google.com/books?id=2fQBGOU4UfQC&pg=PA232&lpg=PA232&dq=NSURLConnection+in+java&source=bl&ots=y7vr0APHHx&sig=6JiH0pH4iZMQNirPZ54LOj8XM7Y&hl=en&sa=X&ved=0CB0Q6AEwAGoVChMIhaja2fyayQIVRJOUCh1H9Qzs#v=onepage&q=NSURLConnection%20in%20java&f=false
-	public URLConnection connection;
+	public Media           media;
+	public Bitmap          data;  // todo Bitmap or ByteBuffer?
+	public URL             url;
+
+	public AsyncHttpClient connection; // allows tracking of live/uncompleted connections
 
 	public Date start;
 	public long time; // see: http://stackoverflow.com/a/16558666
