2035ba76fa517e96dcc3e9c2280995e9d63068c5
Scott Morison <smorison@uoregon.edu>
2016-04-28 12:41:12 -0700

    Last check in of week.

--- app/src/main/java/edu/uoregon/casls/aris_android/Utilities/AppUtils.java
+++ app/src/main/java/edu/uoregon/casls/aris_android/Utilities/AppUtils.java
@@ -1,5 +1,6 @@
 package edu.uoregon.casls.aris_android.Utilities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
@@ -9,6 +10,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
@@ -42,6 +44,8 @@ import edu.uoregon.casls.aris_android.GamesListActivity;
  */
 public class AppUtils {
 
+	private static final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 22;
+
 	public static boolean isNetworkAvailable(Context context) {
 		boolean haveConnectedWifi = false;
 		boolean haveConnectedMobile = false;
@@ -61,11 +65,18 @@ public class AppUtils {
 
 
 	public static Location getGeoLocation(Context context) {
+		if ( ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+			ActivityCompat.requestPermissions( (Activity) context, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+					MY_PERMISSION_ACCESS_COURSE_LOCATION );
+		}
+
 		if ( Build.VERSION.SDK_INT >= 23 &&
-				ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED
+				ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED
 				&& ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//				&& isLocationEnabled(context)
+//								&& isLocationEnabled(context)
 				) {
+			return new Location("0");
+		}
 		// Get LocationManager object from System Service LOCATION_SERVICE
 		LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 		// Create a criteria object to retrieve provider
@@ -74,9 +85,7 @@ public class AppUtils {
 		String provider = locationManager.getBestProvider(criteria, true);
 		// Return Current Location
 		return locationManager.getLastKnownLocation(provider);
-		}
-		// no permission return:
-		return new Location("0");
+
 	}
 
 	public static boolean isLocationEnabled(Context context) {
