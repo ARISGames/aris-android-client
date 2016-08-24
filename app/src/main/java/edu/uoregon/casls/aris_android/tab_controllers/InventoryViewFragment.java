50e80edea5873f8fc2d2e3f26c66b59898907389
Scott Morison <smorison@uoregon.edu>
2016-08-24 14:05:06 -0700

    Inventory list now populates, but needs some work to account for items with a qty of 0. Currently they show up in the list. They shouldn't.

--- app/src/main/java/edu/uoregon/casls/aris_android/tab_controllers/InventoryViewFragment.java
+++ app/src/main/java/edu/uoregon/casls/aris_android/tab_controllers/InventoryViewFragment.java
@@ -11,18 +11,22 @@ import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import edu.uoregon.casls.aris_android.GamePlayActivity;
 import edu.uoregon.casls.aris_android.R;
+import edu.uoregon.casls.aris_android.data_objects.Instance;
 import edu.uoregon.casls.aris_android.data_objects.Item;
 import edu.uoregon.casls.aris_android.data_objects.Media;
+import edu.uoregon.casls.aris_android.models.PlayerInstancesModel;
 
 
 public class InventoryViewFragment extends Fragment {
 
 	private transient GamePlayActivity mGamePlayAct;
 	public View mThisFragsView;
+	public Collection<Instance> mInstances = new ArrayList();
 
 //	private OnFragmentInteractionListener mListener;
 
@@ -44,10 +48,11 @@ public class InventoryViewFragment extends Fragment {
 			mGamePlayAct = (GamePlayActivity) getActivity();
 
 		this.updateList();
+		mInstances = new ArrayList<>();
 		return mThisFragsView;
 	}
 
-	public void updateList() {
+	public void updateList() { // refreshViews in iOS InventoryViewController(
 		LinearLayout llInventoryList = (LinearLayout) mThisFragsView.findViewById(R.id.ll_inventory_list);
 		llInventoryList.removeAllViews(); // refresh visible views so they don't accumulate
 		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
@@ -55,11 +60,17 @@ public class InventoryViewFragment extends Fragment {
 				LinearLayout.LayoutParams.MATCH_PARENT);
 		layoutParams.setMargins(0, -1, 0, -1);
 
-		Collection<Item> itemsList = mGamePlayAct.mGame.itemsModel.items().values();
+		Collection<Instance> instances = mGamePlayAct.mGame.playerInstancesModel.inventory().values();//playerInstances.values();
 
-		if (itemsList == null || itemsList.size() < 1) {
+		// todo: need to examine code to make sure we're looking at the correct instances and to reinstate the qty == 0 filter loop below
+		for (Instance instance : instances) { //60832
+//				if (instance.qty == 0)
+//					continue;
+			mInstances.add(instance);
+		}
+		if (mInstances == null || mInstances.size() < 1) {
 			TextView tvNoItemsMessage = new TextView(mGamePlayAct);
-			tvNoItemsMessage.setText("No Active Quests");
+			tvNoItemsMessage.setText("No Items in Inventory");
 			tvNoItemsMessage.setTextSize(getResources().getDimension(R.dimen.textsize_small));
 			tvNoItemsMessage.setGravity(Gravity.CENTER_HORIZONTAL);
 			tvNoItemsMessage.setPadding(0, 15, 0, 0);
@@ -68,12 +79,12 @@ public class InventoryViewFragment extends Fragment {
 		}
 		// populate with inventory items.
 		else {
-			for (Item item : itemsList) {
+			for (Instance item : mInstances) {
 				LayoutInflater inflater = (LayoutInflater) mGamePlayAct.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				final View itemView = inflater.inflate(R.layout.inventory_list_item, null);
 				// icon/graphic
 				WebView wvItemIcon = (WebView) itemView.findViewById(R.id.wv_inventory_item_icon);
-				if (item.icon_media_id == 0) {
+				if (item.icon_media_id() == 0) {
 					wvItemIcon.setBackgroundColor(0x00000000);
 					wvItemIcon.setBackgroundResource(R.drawable.logo_icon); //todo: default item icon here.
 				}
@@ -82,17 +93,21 @@ public class InventoryViewFragment extends Fragment {
 					wvItemIcon.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
 					wvItemIcon.getSettings().setLoadWithOverviewMode(true); // causes the content (image) to fit into webview's window size.
 					wvItemIcon.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-					Media itemIconMedia = mGamePlayAct.mGameMedia.get(item.icon_media_id);
+//					Media itemIconMedia = mGamePlayAct.mGameMedia.get(item.icon_media_id());
+//					Media media = mGamePlayActivity.mMediaModel.mediaForId(mPlaque.media_id);
+
+					Media itemIconMedia = mGamePlayAct.mMediaModel.mediaForId(item.icon_media_id());
 					String item_icon_URL = itemIconMedia.mediaCD.remoteURL;
 					String iconAsHtmlImg = "<html><body style=\"margin: 0; padding: 0\"><img src=\"" + item_icon_URL + "\" width=\"100%\" height=\"100%\"/></body></html>";
 					wvItemIcon.loadData(iconAsHtmlImg, "text/html", null);
 				}
 				TextView tvItemName = (TextView) itemView.findViewById(R.id.tv_inventory_item_name);
-				tvItemName.setText(item.name);
+				tvItemName.setText(item.name());
 				TextView tvItemDesc = (TextView) itemView.findViewById(R.id.tv_inventory_item_desc);
-				tvItemDesc.setText(item.desc);
+				Item i = (Item) item.object();
+				tvItemDesc.setText(i.desc);
 				TextView tvItemQty = (TextView) itemView.findViewById(R.id.tv_inventory_item_qty);
-//				tvItemQty.setText(item.); // todo: fix this.
+				tvItemQty.setText(String.valueOf(item.qty));
 				llInventoryList.addView(itemView);
 			}
 		}
